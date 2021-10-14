0ccb397d87e047740a04bf11094202699c3695b5
Luis Galindo <egalindo54@uabc.edu.mx>
2021-10-14 14:56:55 -0600

    jue 14 oct 2021 14:57:01

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// https://stackoverflow.com/questions/21479107/saving-html5-textarea-contents-to-file
+function download(){
+    var text = document.getElementById("stdin").value;
+    text = text.replace(/\n/g, "\r\n"); // To retain the Line breaks.
+    var blob = new Blob([text], { type: "text/plain"});
+    var anchor = document.createElement("a");
+    anchor.download = "my-filename.txt";
+    anchor.href = window.URL.createObjectURL(blob);
+    anchor.target ="_blank";
+    anchor.style.display = "none"; // just to be safe!
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+}
