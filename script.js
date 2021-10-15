99ff0432831382488a8a51922a9d2ce49c3fee49
Luis Galindo <egalindo54@uabc.edu.mx>
2021-10-14 18:27:09 -0600

    jue 14 oct 2021 18:27:13

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
 
 // https://stackoverflow.com/questions/21479107/saving-html5-textarea-contents-to-file
+
+function getText() {
+    return document.getElementById("stdin").value;
+}
+
+function getName(){
+    return document.getElementById("file-name").value;
+}
+
 function download(){
-    var text = document.getElementById("stdin").value;
+    var text = getText();
     text = text.replace(/\n/g, "\r\n"); // To retain the Line breaks.
     var blob = new Blob([text], { type: "text/plain"});
     var anchor = document.createElement("a");
-    anchor.download = "my-filename.txt";
+    anchor.download = getName(); //"my-filename.txt";
+    
     anchor.href = window.URL.createObjectURL(blob);
     anchor.target ="_blank";
     anchor.style.display = "none"; // just to be safe!
     document.body.appendChild(anchor);
     anchor.click();
     document.body.removeChild(anchor);
+}
+
+// https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
+   return result;
 }
