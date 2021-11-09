f38c176aae80de5d11e30926f86bf8f724f0d94f
Luis Galindo <egalindo54@uabc.edu.mx>
2021-11-09 09:28:59 -0700

    bug de borde fantasma eliminado

--- script/script.js
+++ script/script.js
@@ -1,8 +1,7 @@
-
-window.onload = () => {
-    $("input").setAttribute("list",window.localStorage.getItem("list"));
-    $("buffer").value = window.localStorage.getItem("buffer");
-};
+function syntaxError() {
+    let error = '<span style="color:red;font-family:courier">';
+    return $("stdout").innerHTML.includes(error)
+}
 
 function removeStdinLastLine() {
     let stdin = $("stdin")
@@ -12,51 +11,16 @@ function removeStdinLastLine() {
     stdin.value = lines.join('\n')
 }
 
-function syntaxError() {
-    let error = '<span style="color:red;font-family:courier">';
-    return $("stdout").innerHTML.includes(error)
-}
 
 function clearInput() {
-    $("input").value = ""    
+    $("input").value = "";
 }
 
 function isCommnad() {
-    return $("input").value.charAt(0) === ':'
+    return $("input").value.charAt(0) === ':';
 }
 
-function executeCommand(){
-    let parts = $("input").value.split(' ')
-    let expr =  parts[0]
-    
-    switch (expr) {
-    case ":alert":
-        alert(parts[1]);
-        break;
 
-    case ":save":
-        alert("save done")
-        window.localStorage.setItem(parts[1], $("buffer").value);
-        break;
-
-    case ":load":
-        $("buffer").value = window.localStorage.getItem(parts[1]);
-        break;
-
-    case ":disable":
-        $("input").setAttribute("list", "");
-        window.localStorage.setItem("list","");
-        break;
-
-    case ":enable":
-        $("input").setAttribute("list", "functions"); 
-        window.localStorage.setItem("list","functions");
-        break;
-        
-    default:
-        alert("Comando error");
-    }
-}
 
 $("input").addEventListener("keyup", (event) => {
     let ENTER_KEY_CODE = 13
@@ -69,7 +33,7 @@ $("input").addEventListener("keyup", (event) => {
         }
 
         if( isCommnad() ) {
-            executeCommand();
+            executeCommand($("input").value);
         } else {
             $("stdin").value += "\n" + $("input").value;
             run();
@@ -81,22 +45,14 @@ $("input").addEventListener("keyup", (event) => {
 });
 
 
-$("input").addEventListener("focus", (event) => {
-    $("buffer").style.display = "none";
-});
 
-$("input").addEventListener("blur", (event) => {
-    $("buffer").style.display = "block";
-});
-
-$("buffer").addEventListener("focus", (event) => {
-    $("input").value = "";
-});
 
 $("buffer").addEventListener("change", () => {
     window.localStorage.setItem("buffer",$("buffer").value);
     $("stdin").value = "trace=1\n" + $("buffer").value;
 });
+
+
 
 $("execute").addEventListener("click", () => {
 
