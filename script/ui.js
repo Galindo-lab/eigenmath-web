99157b7c4d58dcdf52a170becc741442ab9f4e66
Luis Galindo <egalindo54@uabc.edu.mx>
2021-11-14 12:26:45 -0700

    bug 14N21 solucionado

--- script/ui.js
+++ script/ui.js
@@ -5,8 +5,8 @@
 
 function toggleBuffer() {
     let buffer_display = $("buffer").style.display;
-    if(window.screen.availWidth <= 1023) {                
-        if( buffer_display == "block" ) {
+    if(window.screen.availWidth <= 1023) {
+        if(buffer_display == "none") {
             $("buffer").style.display = "block";
         } else {
             $("buffer").style.display = "none";
@@ -18,9 +18,13 @@ function toggleBuffer() {
 // ---------------------------------------------------------
 
 
-$("input").addEventListener("focus", toggleBuffer);
+$("input").addEventListener("focus", () => {
+    toggleBuffer()
+});
 
-$("input").addEventListener("blur", toggleBuffer);
+$("input").addEventListener("blur", () => {
+    toggleBuffer()
+});
 
 $("buffer").addEventListener("focus", (event) => {
     $("input").defaultValue;
