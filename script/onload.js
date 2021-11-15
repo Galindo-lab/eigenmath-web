d9f4e24b78d664f14c9645a75c31820666c6e2f6
Luis Galindo <egalindo54@uabc.edu.mx>
2021-11-15 12:28:05 -0700

    Graficas ajustadas a moviles

--- script/onload.js
+++ script/onload.js
@@ -11,3 +11,25 @@ window.onload = () => {
     $("input").setAttribute("list",window.localStorage.getItem("list"));
     $("buffer").value = window.localStorage.getItem("buffer");
 };
+
+function emit_graph()
+{
+	var h, w;
+
+	h = DRAW_TOP_PAD + DRAW_HEIGHT + DRAW_BOTTOM_PAD;
+	w = DRAW_LEFT_PAD + DRAW_WIDTH + DRAW_RIGHT_PAD;
+
+	h = "height='" + h + "'";
+	w = "width='" + w + "'";
+
+	outbuf = "<svg class='emited_graph' viewbox='50 0 600 350'" + h + w + ">"
+
+	emit_axes();
+	emit_box();
+	emit_labels();
+	emit_points();
+
+	outbuf += "</svg><br>";
+
+	stdout.innerHTML += outbuf;
+}
