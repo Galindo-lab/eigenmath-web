42fcc845c6206bfd9cc9fb61c3785a6a5c3668a9
Luis Galindo <egalindo54@uabc.edu.mx>
2022-04-15 22:33:49 -0700

    Formateo completado

--- script/stdin.js
+++ script/stdin.js
@@ -13,16 +13,13 @@ class stdin {
     document.execCommand("insertText", false, textToInsert);
   }
 
-    // guarda el contenido del buffer en el localstorage, para que no se borre al salir de la aplicacion
+  // guarda el contenido del buffer en el localstorage, para que no se borre al salir de la aplicacion
   static save() {
     window.localStorage.setItem("stdin", stdin.textArea.value);
   }
 
-    // recarga el contenido del buffer
-    static reload() {
-        stdin.textArea.value = window.localStorage.getItem("stdin");
-    }
+  // recarga el contenido del buffer
+  static reload() {
+    stdin.textArea.value = window.localStorage.getItem("stdin");
+  }
 }
-
-
-//function emit_graph()
